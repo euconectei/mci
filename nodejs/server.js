ebb1b345d6380dc33010f16e9bb8059669549f7c
euconectei <rafaelfms@gmail.com>
2018-11-01 08:11:05 -0300

    iniciando server

--- nodejs/server.js
+++ nodejs/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
+console.log(`http://localhost:${port}`);
+
 const mongoose = require('mongoose');
 
 const bodyParser = require('body-parser');
 
 const configDB = require('./config/database.js');
 
-mongoose.connect(configDB.url,{
-  useMongoClient: true
-});
+mongoose.connect(configDB.url), { useNewUrlParser: true };
+
+app.get('/', (req, res) => res.write(JSON.stringify({ nome: 'Rafael' })));
